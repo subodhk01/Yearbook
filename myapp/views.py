7c36e383e41d997a56da9a8de66473a6295501ce
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-28 03:43:10 +0530

    added an exception email

--- myapp/views.py
+++ myapp/views.py
@@ -100,7 +100,7 @@ def index(request):
 @login_required()
 def profile(request):
     email = request.user.email
-    exceptions = ['akul.gupta.phe17@itbhu.ac.in','subodh.verma.min19@itbhu.ac.in']
+    exceptions = ['akul.gupta.phe17@itbhu.ac.in','subodh.verma.min19@itbhu.ac.in','vijay.psingh.chy14@itbhu.ac.in']
     if not(re.findall("15@", email) or re.findall("16@", email)) and not(email in exceptions):
         user = User.objects.get(email=email)
         user.delete()
